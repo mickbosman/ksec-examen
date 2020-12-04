5dc8d04acdeb42788e839860c58d33335514765a
noxion1 <jolopolo3@gmail.com>
2020-12-04 16:29:33 +0100

    nav
    nav + css

--- screens/main.php
+++ screens/main.php
@@ -6,6 +6,14 @@
      <title>VideoBox</title>
    </head>
    <body>
+     <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="#login">Login</a>
+  <a href="#signup">Sign up</a>
+</div>
 
    </body>
  </html>
