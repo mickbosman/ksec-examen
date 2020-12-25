ebdaa4ed242f98660f7fe4e75a3564342a06ec44
noxion1 <jolopolo3@gmail.com>
2020-12-25 13:41:29 +0100

    nav fixed

--- screens/account/header.php
+++ screens/account/header.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../../css/style.css">
     <meta charset="utf-8">
     <title>VideoBox</title>
   </head>
   <body>
     <div class="topnav">
- <a href="home.php" class="active">Home</a>
+ <a href="../upload/video.php">Home</a>
  <a href="news.php">News</a>
  <a href="contact.php">Contact</a>
  <a href="about.php">About</a>
- <a href="../account/login.php">Login</a>
+ <a href="login.php">Login</a>
  <a href="signup.php">Sign up</a>
 </div>
